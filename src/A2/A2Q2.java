f14538457032b50b3829e24e75a467bd3f8bb927
NamDo8467 <huynhnamdo@gmail.com>
2024-02-19 21:01:36 -0500

    complete assignment

--- src/A2/A2Q2.java
+++ src/A2/A2Q2.java
@@ -8,6 +8,8 @@ package A2;
 // Do NOT change the package!
 
 import java.util.ArrayList;
+
+import A2.A2Q2.Move;
 // Do NOT add any import!
 /**
  * The A2Q2 class
@@ -75,15 +77,30 @@ public class A2Q2 {
     public static void threePegTOHHelper(int n, ArrayList<Move> moves, int from, int to, int auxiliary) {
     	if(n == 1) {
     		moves.add(new Move(from, to));
-    		System.out.println(from +" " + " " + to);
+    		System.out.println(from +" " + " " + to+ " ");
     		return;
     	}
-    	threePegTOHHelper(n-1, moves, from, auxiliary, to);
-    	moves.add(new Move(from, to));
+    	//if n = 3 then
+    	threePegTOHHelper(n-1, moves, from, auxiliary, to); // n=2; from = A, to = C, auxiliary = B -- n=1; from = A, to = B, auxiliary = C
+    	moves.add(new Move(from, to)); // from = 1, to = 2
     	System.out.println(from +" " + " " + to);
-    	threePegTOHHelper(n-1, moves, auxiliary, to,from );
+    	threePegTOHHelper(n-1, moves, auxiliary, to,from ); // n = 2; from = C, to = B, auxiliary = A --- n = 1; from = A, to = B, aux = C
     	
-    	
+    }
+    public static void fourPegTOHHelper(int n, ArrayList<Move> moves, int from, int to, int aux1, int aux2) { // from = 1, to = 2, aux1 = 3, aux2 = 4
+    	if(n == 0) {
+    		return;
+    	}
+    	if(n == 1) {
+    		moves.add(new Move(from, to));
+    		return;
+    	}
+    	int k = 2;
+    	fourPegTOHHelper(n-k, moves, from, aux1, aux2, to); // n - k = 3, from = 1, to = 3, aux1 = 4, aux2 = 2
+    	moves.add(new Move(from, aux2));
+    	moves.add(new Move(from, to));
+    	moves.add(new Move(aux2, to));
+    	fourPegTOHHelper(n-k, moves, aux1, to, from, aux2);
     }
 
     /**
@@ -95,7 +112,6 @@ public class A2Q2 {
      * - n > 0
      */
     public static ArrayList<Move> threePegTOH(int n) {
-        // TODO: Complete this method
     	ArrayList<Move> moves = new ArrayList<Move>();
     	threePegTOHHelper(n,moves, 1,2,3);
    
@@ -113,14 +129,16 @@ public class A2Q2 {
      * - n > 0
      */
     public static ArrayList<Move> fourPegTOH(int n) {
-
-        // TODO: Complete this method
-        return null; // Placeholder return statement. Should be changed when the method is implemented..
+    	ArrayList<Move> moves = new ArrayList<Move>();
+    	fourPegTOHHelper(n, moves, 1,2,3,4);
+    	return moves;
+//        return null; // Placeholder return statement. Should be changed when the method is implemented..
     }
 
     public static void main(String[] args) {
 //        System.out.println(threePegTOH(3));
 //        System.out.println(fourPegTOH(3));
     	threePegTOH(3);
+    	fourPegTOH(5);
     }
 }
