a1d3ee1cdc47924c9269febbb1d8408cf9d837d9
NamDo8467 <huynhnamdo@gmail.com>
2024-05-12 22:18:56 -0400

    finish assignment

--- src/A3/A3Q2.java
+++ src/A3/A3Q2.java
@@ -11,12 +11,69 @@ package A3;
  * Your own implementation of a HashTable class.
  * Add the appropriate fields and methods you want in order to solve this problem.
  */
+class Node {
+	public int key;
+	public int value;
+	public Node next;
+	public Node(int key, int value, Node next) {
+		this.key = key;
+		this.value = value;
+		this.next = next;
+	}
+}
 class HashTable {
 
     // TODO: implement your own HashTable class
+	private Node table[];
+	private int i = 0; // this variable is used for linear probing
+	
+	public HashTable(int size) {
+		this.table = new Node [size];
+		
+	}
+	public int hash(int key) {
+		if(key < 0) {
+			key = key *(-1);
+		}
+		return key%table.length;
+	}
+	public void put(int key, int value) {
+		int index = hash(key);
+		Node node = table[index];
+	
+		while(node!= null) {
+			if(node.key == key) {
+				node.value = value;
+				return;
+			}
+			node = node.next;
+		}
+		table[index] = new Node(key, value, table[index]);
+		
+	}
+	public int getOrDefault(int key, int defaultValue) {
+		int index = hash(key);
+		Node node = table[index];
+		while(node!= null) {
+			if(node.key == key) {
+				return node.value;
+			}
+			node = node.next;
+		}
+		return defaultValue;
+	}
+//	public void print() {
+//		for(int i = 0; i < table.length; i++) {
+//			System.out.print(table[i]);
+//		}
+//	}
+//	
+	
 }
 public class A3Q2 {
-
+	public static int RANGE = 50;
+	public static int TABLE_SIZE = (RANGE + RANGE) * (RANGE + RANGE);
+	
     /**
      * Return the number of unique integer solutions where each integer is in the range [-50, 50].
      * The equation is given in the handout.
@@ -28,7 +85,32 @@ public class A3Q2 {
     public static long solve(int A, int B, int C, int D, int E, int S) {
 
         // TODO: implement this function
-        return 0;
+//    	 HashMap<Integer, Integer> table = new HashMap<>();
+    	HashTable table = new HashTable(TABLE_SIZE);
+//    	 HashTable table2 = new HashTable(TABLE_SIZE);
+//    	 table2.print();
+//    	 int RANGE = 50;
+         for (int x1 = -RANGE; x1 <= RANGE; x1++) {
+             for (int x2 = -RANGE; x2 <= RANGE; x2++) {
+                 int sum = A * x1 + B * (int) Math.pow(x2, 2);
+                 table.put(sum, table.getOrDefault(sum, 0) + 1);
+             }
+         }
+         
+         long count = 0; // This will hold the number of solutions.
+         
+         // Phase 2: Check if the sums of C*x3^3 + D*x4^4 + E*x5^5 exist in the hash table.
+         for (int x3 = -RANGE; x3 <= RANGE; x3++) {
+             for (int x4 = -RANGE; x4 <= RANGE; x4++) {
+                 for (int x5 = -RANGE; x5 <= RANGE; x5++) {
+                     int sum = C * (int) Math.pow(x3, 3) + D * (int) Math.pow(x4, 4) + E * (int) Math.pow(x5, 5);
+                     count += table.getOrDefault(S - sum, 0);
+                 }
+             }
+         }
+         
+         return count;
+
     }
 
     public static void main(String[] args) {
@@ -40,5 +122,7 @@ public class A3Q2 {
         System.out.println(340 == solve(12, 34, 56, 78, 9, 10));
         System.out.println(16665 == solve(20, -11, -2, 0, 11, -2011));
         System.out.println(10510100501L == solve(0, 0, 0, 0, 0, 0));
+    	
+    	
     }
 }
